fabb0fd4d545801e2f70429bbdd4576792ff26f5
Aula Randstad <Aula Randstad@PC2225>
2017-01-23 12:11:56 +0100

    FirstCommit

---
+++ SaleClassLibrary/ISaleService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace SaleClassLibrary
+{
+    [ServiceContract]
+    interface ISaleService
+    {
+        [OperationContract]
+        bool InsertCustomer(Customer obj);
+
+        [OperationContract]
+        List<Customer> GetAllCustomer();
+
+        [OperationContract]
+        bool DeleteCustomer(int Cid);
+
+        [OperationContract]
+        bool UpdateCustomer(Customer obj);
+
+    }
+
+    [DataContract]
+    public class Customer
+    {
+        [DataMember]
+        public int CustomerID;
+        [DataMember]
+        public string CustomerName;
+        [DataMember]
+        public string Address;
+        [DataMember]
+        public string EmailId;
+    }
+}
---
+++ SaleClassLibrary/SaleService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaleClassLibrary
+{
+    class SaleService : ISaleService
+    {
+        public bool DeleteCustomer(int Cid)
+        {
+            var item = customerList.First(x => x.CustomerID == Cid);
+
+            customerList.Remove(item);
+            return true;
+        }
+
+        public List<Customer> GetAllCustomer()
+        {
+            return customerList;
+        }
+
+        public bool InsertCustomer(Customer obj)
+        {
+            customerList.Add(obj);
+            return true;
+        }
+
+        public bool UpdateCustomer(Customer obj)
+        {
+            customerList.Where(p=> p.CustomerID == obj.CustomerID).Update(p => p.CustomerName = obj.CustomerName);
+            return true;
+        }
+
+
+        public static List<Customer> customerList = new List<Customer>()
+        {
+            new Customer { CustomerID = 1, CustomerName = "Sujeet",
+            Address = "Pune", EmailId = "test@yahoo.com" },
+            new Customer { CustomerID = 2, CustomerName = "Rahul",
+            Address = "Pune", EmailId = "test@yahoo.com" }
+
+        };
+    }
+
+    public static class LinqUpdate
+    {
+        public static void Update<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var item in source)
+                action(item);
+
+        }
+
+    }
+
+}
+    
+
+    
